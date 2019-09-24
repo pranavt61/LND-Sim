e080954a64f3d396acaf36bf5a0cc01a10208707
Ubuntu <ubuntu@ip-172-31-16-98.us-west-2.compute.internal>
2019-09-24 02:09:07 +0000

    Make RPC connection, Gen seed, and Init Wallet

--- scripts/lnd-cluster.py
+++ scripts/lnd-cluster.py
@@ -4,6 +4,15 @@ from multiprocessing import Process
 import subprocess
 import os
 import shutil
+import rpc_pb2 as ln
+import rpc_pb2_grpc as lnrpc
+import grpc
+
+import base64
+import json
+import requests
+import time
+import codecs
 
 NEXT_NODE_ID = 0
 MAX_NODES = 10
@@ -16,8 +25,7 @@ start_up = """ lnd
     --rpclisten=localhost:{} 
     --listen=localhost:{} 
     --restlisten=localhost:{} 
-    --datadir={} 
-    --logdir={} 
+    --lnddir={}
     --debuglevel=debug 
     --bitcoin.simnet 
     --bitcoin.active 
@@ -28,9 +36,29 @@ start_up = """ lnd
 def main():
     node_threads = [];
 
-    for node_id in range(0, 3):
+    for node_id in range(0, 1):
         node_threads.append(Process(target=start_node, args=(node_id,)))
         node_threads[node_id].start()
+
+    time.sleep(1)
+
+    os.environ['GRPC_SSL_CIPHER_SUITES'] = 'HIGH+ECDSA'
+
+    # init stubs
+    cert = open(NODES_DIR + '/0/tls.cert', 'rb').read()
+    ssl_creds = grpc.ssl_channel_credentials(cert)
+    channel = grpc.secure_channel('localhost:10000', ssl_creds)
+    stub = lnrpc.WalletUnlockerStub(channel)
+
+    request = ln.GenSeedRequest()
+    response = stub.GenSeed(request)
+    print(response.cipher_seed_mnemonic)
+
+    request = ln.InitWalletRequest(
+            wallet_password="00000000",
+            cipher_seed_mnemonic=response.cipher_seed_mnemonic)
+    responce = stub.InitWallet(request)
+    print(response)
 
     return
 
@@ -39,8 +67,6 @@ def start_node(node_id):
     node_peer = str(10010 + node_id)
     node_rest = str(8000 + node_id)
     node_dir = NODES_DIR + '/' + str(node_id)
-    node_data = node_dir + "/data"
-    node_logs = node_dir + "/logs"
 
     # delete dir
     dir_list = os.listdir(NODES_DIR)
@@ -49,11 +75,9 @@ def start_node(node_id):
 
     # create dir
     os.mkdir(node_dir)
-    os.mkdir(node_data)
-    os.mkdir(node_logs)
 
     # start lnd
-    lnd_cmd = start_up.format(node_rpc, node_peer, node_rest, node_data, node_logs)
+    lnd_cmd = start_up.format(node_rpc, node_peer, node_rest, node_dir)
     lnd_output = cmd(lnd_cmd)
 
     for l in lnd_output:
